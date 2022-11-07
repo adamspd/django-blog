730497eca57c5a03439553b953287ffac44110f5
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2022-11-07 15:34:38 +0100

    readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # django-blog
 A package that helps integrate a django blog in your website project
+
+Before launching the project, make sure to create migrations and run them. Otherwise, the new changes won't take effect 
+and you will have a bunch of errors.
+
+New changes include :
+    Added 'django.contrib.sites', to settings
+
+After finishing the migrations go to "/admin" of your site, in the sites section, change "example.com" to your site's
+url. Always modify, never add a new one, otherwise, it will break.
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -35,12 +35,12 @@ DEBUG = production_debug
 ALLOWED_HOSTS = ["*"] if DEBUG else ['https://adamspierredavid.com', 'www.adamspierredavid.com', 'adamspierredavid.com',
                                      '144.217.7.48']
 
-
 # Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -84,7 +84,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_blog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -94,7 +93,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -114,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -125,7 +122,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
@@ -238,7 +234,6 @@ LANGUAGES_BIDI = global_settings.LANGUAGES_BIDI + ["cr-ht"]
 LOCALE_PATHS = (
     os.path.join(BASE_DIR / 'locale'),
 )
-
 
 LOGGING = {
     'version': 1,
